7c5b4845e75b3cc115977c418b821da6b65e3655
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-11 16:30:06 +0200

    create nav

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,10 +1,38 @@
 <template>
-  <header>header</header>
+  <header>
+    <nav>
+      <ul>
+        <li v-for="(item, index) in navMenu" :key="index">
+          <router-link :to="{ name: item.navLink }">{{
+            item.label
+          }}</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
   name: "HeaderComponent",
+  data() {
+    return {
+      navMenu: [
+        {
+          navLink: "home",
+          label: "Home",
+        },
+        {
+          navLink: "blog",
+          label: "Blog",
+        },
+        {
+          navLink: "contatti",
+          label: "Contatti",
+        },
+      ],
+    };
+  },
 };
 </script>
 
