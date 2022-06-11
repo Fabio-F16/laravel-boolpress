d0ba4af135f9df704a7522dbd3bf93fd81e01f82
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-11 16:31:23 +0200

    NotFoundPage

---
+++ resources/js/pages/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>404 - Pagina non trovata</h1>
+    <div>
+      <router-link :to="{ name: 'home' }">Vai alla homePage </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+};
+</script>
+
+<style>
+</style>
