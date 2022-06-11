08f8207c07c30ad3ae80f21fad2a012e1fd1bc24
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-11 16:30:21 +0200

    PostCardComponent

---
+++ resources/js/components/PostCardComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img :src="'storage/' + cover" :alt="title" />
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostCardComponent",
+  props: ["title", "cover"],
+};
+</script>
+
+<style>
+</style>
